433b2cb1630bb00b712f93c78ab0cd921cb74c6d
Stuti Kaushik <kaushikstuti4@gmail.com>
2024-08-21 14:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	function firstNonRepeatedChar(str) {
+  
+//  * create a frequency map; where we find all the frequency for each character
+const charFreq = {}; // charFreq["Name of Field"] 
+for(let char of str) {
+    charFreq[char] = (charFreq[char] || 0) + 1; // Language Specific || JS 
+}
+ 
+
+for (let char of str) {
+    if(charFreq[char] === 1) {
+        return char
+    }
+}
+return null;
+}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
